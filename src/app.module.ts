9ac0c4cff451826e0d4110c390592f9e1fc8e3b6
Carl Meyer <carl@ukiyo-studio.co.uk>
2023-03-05 17:34:21 +0000

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'host.docker.internal',
+      port: 3306,
+      username: 'dev',
+      password: 'password',
+      database: 'minibreakdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
